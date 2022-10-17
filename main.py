1b475455aea5c15f1fcbd523391be7d9d41cbc7f
Denys Shcherbak <denys.shcherbak.official@gmail.com>
2022-10-17 20:19:09 +0300

    Implemented polish notation for regex

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-from RegexElement import RegexElement, RegexElemType
+from RegexElement import RegexElement, RegexElemType, create_from_text
 import NFA
-from transition_table import build_transition_table, print_transition_table
+#from transition_table import build_transition_table, print_transition_table
 
 def main():
     # get regex
-
+    
+    regex = create_from_text("abVba{a}")
     # regex to enum list
    
-    regex = [RegexElement(RegexElemType.Letter, "a"), 
-            RegexElement(RegexElemType.Letter, "b"), 
-            RegexElement(RegexElemType.Conjunction)]
+    # regex = [RegexElement("a"), RegexElement("b"), RegexElement('V')]
 
     # build automata
 
     nfa = NFA.compileNFA(regex)
+    print("hello")
     # output automata table
-    table = build_transition_table(nfa)
-    print_transition_table(table)
+   # table = build_transition_table(nfa)
+    #print_transition_table(table)
 
 main()
